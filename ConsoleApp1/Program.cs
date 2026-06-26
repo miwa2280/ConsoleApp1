e5a212abef26bc7e646ebd136415fb2fb9184df1
miwa2280 <houseyoupol@gmail.com>
2026-06-26 19:14:55 +0300

    Rename namespace from ConsoleApp1 to Homework1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ConsoleApp1
+
+namespace Homework1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            Console.WriteLine("Лукоморья дуб зелёный;");
+            Console.WriteLine("Златая цепь на дубе том:");
+         
+            Console.WriteLine("И днём и ночью кот учёный");
+            Console.WriteLine("Всё ходит по цепи кругом;");
+
+            Console.Write("Идёт направо — песнь заводит,\n");
+            Console.Write("Налево — сказку говорит.\n");
+
+            Console.WriteLine("Там чудеса: там леший бродит");
+            Console.WriteLine("Русалка сидит на ветвях...");
+
+            Console.ReadLine();
         }
     }
 }

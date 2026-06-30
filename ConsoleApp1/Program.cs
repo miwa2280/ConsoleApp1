71b3157d7f7808ad48b103b168318524d0eaa863
miwa2280 <houseyoupol@gmail.com>
2026-06-30 12:16:57 +0300

    Update Program.cs
    Updated namespace and modified main method to display personal information.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConsoleApp1
+namespace Homework3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Миша";
+            int age = 15;
+            string height = "1.78 м"; 
+            char favoriteLetter = 'М';
+            bool likesProgramming = true;
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Возраст: " + age);
+            Console.WriteLine("Рост: " + height);
+            Console.WriteLine("Любимая буква: " + favoriteLetter);
+            Console.WriteLine("Любит ли программирование: " + (likesProgramming ? "Да" : "Нет"));
+            Console.ReadLine();
         }
     }
 }

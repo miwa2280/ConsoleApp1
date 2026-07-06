7cae3c609a5312ad0a60f853bda2790f9bd0c1cd
miwa2280 <houseyoupol@gmail.com>
2026-07-06 12:13:32 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp1
+namespace homework4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение d: ");
+            int d = int.Parse(Console.ReadLine());
+
+            double result = (((double)a * b / 2) + c * d) / 3;
+
+            Console.WriteLine($"Результат = {result}");
+
+            Console.ReadLine();
         }
     }
 }
